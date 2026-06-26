c80a3dec6de089e1debcbdb7fe5f7e4a06485057
Throwable <115081545+ThrowableCube@users.noreply.github.com>
2026-06-26 23:14:45 +0700

    Update compile.bat
    included flags for library and include folder

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 437
-g++ unogame_win32.cpp -lpdcurses -o uno_win32 > log.txt 2>&1
+g++ unogame_win32.cpp -L./lib -lpdcurses -I./include -o uno_win32 > log.txt 2>&1
 type log.txt
 pause
 uno_win32
